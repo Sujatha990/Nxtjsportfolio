c6673068aee67f3977d16b4b645b3d6ec3b89f5f
Sujatha99 <suji84863@gmail.com>
2024-04-16 13:38:51 +0530

    first commit

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,5 +1,9 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
+import "./MainApp.css";
+import "./_components/Navbar.css";
+import Layout from "./_components/Layout";
+import Navbar from "./_components/Navbar";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -11,7 +15,13 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <div>
+          <Layout />
+          <Navbar/>
+          {children}
+        </div>
+      </body>
     </html>
   );
 }
